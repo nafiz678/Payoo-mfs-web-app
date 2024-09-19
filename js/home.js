cc8c6a80d13f36e0ebc86551507e3a9b23a2f4a6
MD Nafizul Iqram <nafizulikram3@gmail.com>
2024-09-19 18:44:31 +0600

    New section added

--- js/home.js
+++ js/home.js
@@ -5,14 +5,37 @@
  * prevent page reload after page submit
  * S-2: Get money to be added to the account balance
  * S-3: Get the pin number 
+ * S-4: Verify the pin number
  */
 document.getElementById("btn-add-money").addEventListener("click", function(event){
     event.preventDefault();
-    console.log("added event");
 
     //S-2: Get money to be added to the account balance
     const addMoneyInput = document.getElementById("input-add-money").value;
     //S-3: Get the pin number 
     const pinNumberInput = document.getElementById("input-pin-number").value;
     console.log(addMoneyInput, pinNumberInput);
+
+    //S-4: Verify the pin number
+    //wrong way to validate pin number
+    if(pinNumberInput === "12345")
+    {
+        console.log("adding money");
+        //Step-5 :get the balance money
+        const balance = document.getElementById("account-balance").innerText;
+        // balance = balance + addMoneyInput;
+
+        //convert balance string to number
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+        
+        //Step 6 : Update the balance in the UI/DOM
+        document.getElementById("account-balance").innerText = newBalance;
+    }
+    else {
+        
+        alert("successfully failed")
+    }
 })
